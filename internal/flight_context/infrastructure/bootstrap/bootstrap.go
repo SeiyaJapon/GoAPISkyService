3156803119337ac1626202e5711d0350a3749e73
Fran <francisco.perez@bolttech.io>
2025-06-02 13:04:12 +0200

    TODO: Complete RegisterCommands on Bootstrap

--- internal/flight_context/infrastructure/bootstrap/bootstrap.go
+++ internal/flight_context/infrastructure/bootstrap/bootstrap.go
@@ -3,11 +3,12 @@ package bootstrap
 import "GoAPISkyService/internal/flight_context/infrastructure/bootstrap/bus"
 
 func RegisterCommands(commandBus *bus.InMemoryCommandBus, commandHandlers ...interface{}) {
-	for _, handler := range commandHandlers {
-		if cmdHandler, ok := handler.(bus.); ok {
-			commandBus.RegisterHandler(cmdHandler.CommandType(), cmdHandler)
-		} else {
-			panic("Invalid command handler type: " + handler.(string))
-		}
-	}) {
+	// TODO: comnplete RegisterCommands
+	// for _, handler := range commandHandlers {
+	// 	if cmdHandler, ok := handler.(bus.); ok {
+	// 		commandBus.RegisterHandler(cmdHandler.CommandType(), cmdHandler)
+	// 	} else {
+	// 		panic("Invalid command handler type: " + handler.(string))
+	// 	}
+	// }) {
 }

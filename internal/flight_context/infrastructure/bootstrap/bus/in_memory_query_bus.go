b5b10b7ea1cbbc2185b867cab8a9aeb70d23ae95
Fran <francisco.perez@bolttech.io>
2025-06-02 23:52:58 +0200

    bootstrap

--- internal/flight_context/infrastructure/bootstrap/bus/in_memory_query_bus.go
+++ internal/flight_context/infrastructure/bootstrap/bus/in_memory_query_bus.go
@@ -7,25 +7,25 @@ import (
 )
 
 type InMemoryQueryBus struct {
-	handlers map[reflect.Type]query.QueryHandlerInterface
+	handlers map[string]query.QueryHandlerInterface
 }
 
 func NewInMemoryQueryBus() *InMemoryQueryBus {
 	return &InMemoryQueryBus{
-		handlers: make(map[reflect.Type]query.QueryHandlerInterface),
+		handlers: make(map[string]query.QueryHandlerInterface),
 	}
 }
 
 func (bus *InMemoryQueryBus) RegisterHandler(query query.QueryInterface, handler query.QueryHandlerInterface) {
-	queryType := reflect.TypeOf(query)
+	queryType := reflect.TypeOf(query).String()
 	if _, exists := bus.handlers[queryType]; exists {
-		panic("Query handler already registered for query type: " + queryType.String())
+		panic("Query handler already registered for query type: " + queryType)
 	}
 	bus.handlers[queryType] = handler
 }
 
 func (bus *InMemoryQueryBus) Ask(query query.QueryInterface) (query.QueryResultInterface, error) {
-	queryType := reflect.TypeOf(query)
+	queryType := reflect.TypeOf(query).String()
 	handler, exists := bus.handlers[queryType]
 	if !exists {
 		return nil, exceptions.ErrQueryHandlerNotFound

a13eb287bf0fde3e07175e15c7e161bec437b5b3
Fran <francisco.perez@bolttech.io>
2025-06-09 22:15:52 +0200

    init_cqrs

--- internal/flight_context/infrastructure/bootstrap/bootstrap.go
+++ internal/flight_context/infrastructure/bootstrap/bootstrap.go
@@ -1,12 +1,17 @@
 package bootstrap
 
 import (
+	"GoAPISkyService/internal/flight_context/infrastructure/bootstrap/cqrs"
+	"GoAPISkyService/internal/flight_context/infrastructure/bootstrap/cqrs/bus"
 	"GoAPISkyService/internal/flight_context/infrastructure/bootstrap/db"
 	"database/sql"
+	"reflect"
 )
 
 type AppDependencies struct {
-	DB *sql.DB
+	DB         *sql.DB
+	CommandBus *bus.InMemoryCommandBus
+	QueryBus   *bus.InMemoryQueryBus
 }
 
 func InitDependencies() (*AppDependencies, error) {
@@ -15,7 +20,27 @@ func InitDependencies() (*AppDependencies, error) {
 		return nil, err
 	}
 
+	commandBus := bus.NewInMemoryCommandBus()
+	queryBus := bus.NewInMemoryQueryBus()
+
+	for _, registration := range cqrs.CommandHandlers {
+		commandBus.RegisterHandler(
+			reflect.TypeOf(registration.Command),
+			registration.Handler,
+		)
+	}
+
+	// Registrar todas las queries
+	for _, registration := range cqrs.QueryHandlers {
+		queryBus.RegisterHandler(
+			reflect.TypeOf(registration.Query),
+			registration.Handler,
+		)
+	}
+
 	return &AppDependencies{
-		DB: dbConfig,
+		DB:         dbConfig,
+		CommandBus: commandBus,
+		QueryBus:   queryBus,
 	}, nil
 }
